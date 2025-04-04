9f90b042e43cdb08028ba8f8207e3e007db1fee4
Najumudeen M <najuvia@gmail.com>
2025-04-04 09:35:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,9 +12,16 @@ provider "aws" {
     skip_credentials_validation = true
     skip_metadata_api_check = true
     skip_requesting_account_id = true
+    s3_use_path_style           = true
 
     endpoints {
-      ec2 = "http://localhost:4566"
+      ec2            = "http://localhost:4566"
+      s3             = "http://s3.localhost.localstack.cloud:4566"
+      cloudwatch     = "http://localhost:4566"
+      cloudwatchlogs = "http://localhost:4566"
+      iam            = "http://localhost:4566"
+      lambda         = "http://localhost:4566"
+      sts            = "http://localhost:4566"
     }
   
 }
@@ -27,3 +34,22 @@ resource "aws_instance" "web-server" {
     Name = "web-server-${count.index}"
   }
 }
+
+# Create a bucket
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "my-bucket"
+}
+
+resource "aws_s3_bucket_acl" "test-bucket-acl" {
+  bucket = aws_s3_bucket.test-bucket.id
+  acl = "public-read" # or can be "private"
+}
+
+# resource "aws_s3_bucket_notification" "s3_lambda_trigger" {
+#   bucket = aws_s3_bucket.test-bucket.id
+#   lambda_function {
+#     lambda_function_arn = aws_lambda_function.s3_lambda.arn
+#     events = ["s3:ObjectCreated:*"]
+
+#   }
+# }
